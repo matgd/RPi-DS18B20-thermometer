8afd18b9ea9de42bf6c8678845d96c24635d7efa
matgd <matgd@users.noreply.github.com>
2019-02-14 19:38:52 +0100

    PHP version

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Termometr</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js" defer></script>
+</head>
+<body>
+    <div id="temp" class="center">
+        <?php
+            $filepath = '/sys/bus/w1/devices/28-0316b317eaff/w1_slave';
+            $degree = "°C";
+            
+            $file = fopen($filepath, "r") or die("Unable to open file!");
+            $data = fread($file, filesize($filepath));
+            $temp = strchr($data, "t=");
+            $temp = preg_replace("/t=/", "", $temp);
+        
+            echo sprintf("%.3f".$degree, $temp/1000);
+
+            fclose($file);
+
+            # header("refresh:1;url=index.php");
+        ?>
+    </div>
+</body> 
+</html>
